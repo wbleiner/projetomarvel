5dadd33b6f605871cddddcfd31faee2cc922d159
Wanderson Bleiner <wanderson.bleiner@gmail.com>
2021-08-23 15:37:23 -0300

    Mudanças no perfil

--- src/Views/Character.jsx
+++ src/Views/Character.jsx
@@ -15,10 +15,15 @@ const Character = props => {
     }, [id])
     console.log(character);
     return (
-        <div className="Character">
-            
-          <img className="ImageProfile" src={`${character?.thumbnail?.path ?? ""}/standard_fantastic.${character?.thumbnail?.extension ?? ""}` } alt ={character.name}/>
-          <h1 className ="Name Profile">{character.name}</h1>
+        <div className ="Character">
+            <div className="Profile">
+                <img className="ImageProfile" src={`${character?.thumbnail?.path ?? ""}/standard_fantastic.${character?.thumbnail?.extension ?? ""}`} alt={character.name} />
+                <h1 className="NameProfile">{character.name}</h1>
+            </div>
+            <div className="Description">
+                <h1>Description</h1>
+                <p>{character.description}</p>
+            </div>
         </div>
     )
 }
